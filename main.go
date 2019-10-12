f57ffcb257442a4cf999049b677af22ce2bd7db1
Jakub Vítek <sognus@localhost.localdomain>
2019-10-12 22:20:55 +0200

    Komunikace na několika socketech bez vláken

--- main.go
+++ main.go
@@ -6,7 +6,29 @@ import (
 	"syscall"
 )
 
+func FD_SET(p *syscall.FdSet, i int) {
+	p.Bits[i/64] |= 1 << uint(i) % 64
+}
+
+func FD_ISSET(p *syscall.FdSet, i int) bool {
+	return (p.Bits[i/64] & (1 << uint(i) % 64)) != 0
+}
+
+func FD_ZERO(p *syscall.FdSet) {
+	for i := range p.Bits {
+		p.Bits[i] = 0
+	}
+}
+
+type Client struct {
+	socketDescriptor int
+	address          syscall.Sockaddr
+}
+
 func main() {
+	const PORT = 8080
+	var ADDRESS = [4]byte{192, 168, 0, 108}
+
 	fmt.Println("Starting server")
 
 	main_socket, socket_err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
@@ -18,8 +40,8 @@ func main() {
 	fmt.Println("Created main socket!")
 
 	address := syscall.SockaddrInet4{
-		Port: 8081,
-		Addr: [4]byte{192, 168, 0, 109},
+		Port: PORT,
+		Addr: ADDRESS,
 	}
 
 	bind_err := syscall.Bind(main_socket, &address)
@@ -28,39 +50,112 @@ func main() {
 		fmt.Println("Could not bind given IP adress!")
 		os.Exit(2)
 	}
-	fmt.Printf("IP adress %d.%d.%d.%d was bound!\n", address.Addr[0], address.Addr[1], address.Addr[2], address.Addr[3])
+	fmt.Printf("IP adress %d.%d.%d.%d:%d was bound!\n", address.Addr[0], address.Addr[1], address.Addr[2], address.Addr[3], address.Port)
 
 	listen_err := syscall.Listen(main_socket, 10);
-
 	if listen_err != nil {
 		fmt.Println("Could not start listener for given IP adress!")
 		os.Exit(3)
 	}
 	println("Listener was started!\n")
 
+	// File descriptor set
+	rfds := &syscall.FdSet{}
+	timeout := &syscall.Timeval{}
+	clients := make([]Client, 0, 10)
+
+	// Print information
+	println("Accepting new clients...")
+
+	// Main communication Loop
 	for {
-		println("Accepting new connection...")
-		new_socket, new_socket_info , accept_err := syscall.Accept(main_socket)
+		// Reset FD SET
+		FD_ZERO(rfds)
 
-		if accept_err != nil {
-			println("Error occured while accepting new connection: ", accept_err)
+		// Add main socket to socket set
+		FD_SET(rfds, main_socket)
+
+		max_sd := -1
+
+		// Add established clients to socket set
+		for i := 0; i < len(clients); i++ {
+			socketFileDescriptor := clients[i].socketDescriptor
+			FD_SET(rfds, socketFileDescriptor)
+			// Find out highest socket file descriptor
+			if socketFileDescriptor > max_sd {
+				max_sd = socketFileDescriptor
+			}
 		}
 
-		msg := []byte("Nazdar, jsi připojen k serveru!");
+		// TODO: find out what first parameter means
+		_, errSelect := syscall.Select(max_sd+1, rfds, nil, nil, timeout)
 
-		fmt.Println("Client socket file descriptor: ", new_socket)
-		fmt.Println("Client port: ", new_socket_info)
-
-		n, write_err := syscall.Write(new_socket, msg)
-
-		if write_err != nil {
-			println("Could not send message to client!")
+		// Check if we got error
+		if errSelect != nil {
+			fmt.Printf("Select error: %s\n", errSelect.Error())
 		}
 
-		if n != len(msg) {
-			println("Message was sent but didnt arrive whole!")
+		// Check communication for main socket
+		if FD_ISSET(rfds, main_socket) {
+			newSocketDescriptor, newAddress, errAccept := syscall.Accept(main_socket)
+
+			// Check for accept error
+			if errAccept != nil {
+				fmt.Printf("Accept error: %s\n", errAccept.Error())
+			}
+
+			// Create new client record
+			newClient := Client{
+				socketDescriptor: newSocketDescriptor,
+				address:          newAddress,
+			}
+
+			// Print information about new client
+			address := newAddress.(*syscall.SockaddrInet4)
+			fmt.Printf("New client accepted: %d.%d.%d.%d:%d\n", address.Addr[0], address.Addr[1], address.Addr[2], address.Addr[3], address.Port)
+
+			// Sent greetings message to new client
+			msg := "Připojen k serveru\n"
+			_, errWrite := syscall.Write(newSocketDescriptor, []byte(msg))
+
+			// Check for errors
+			if errWrite != nil {
+				fmt.Printf("Greet message write error for %d: %s\n", newSocketDescriptor, errWrite.Error())
+			}
+
+			// Save new client in storage
+			clients = append(clients, newClient)
 		}
 
+		// Check for communication from other clients
+		for i := 0; i < len(clients); i++ {
+			clientReadSocketDescriptor := clients[i].socketDescriptor
+
+			if FD_ISSET(rfds, clientReadSocketDescriptor) {
+				buffer := make([]byte, 10)
+				_, _, errRecv := syscall.Recvfrom(clientReadSocketDescriptor, buffer, 0)
+
+				// Check receive error
+				if errRecv != nil {
+					fmt.Printf("Receive error: %s\n", errSelect.Error())
+				}
+
+				// Send to other clients
+				for y := 0; y < len(clients); y++ {
+					clientWriteSocketDescriptor := clients[y].socketDescriptor
+					msg := fmt.Sprintf("<Client %d> %s\n", clientReadSocketDescriptor, buffer)
+					// Todo: find out what is first value
+					_, errWrite := syscall.Write(clientWriteSocketDescriptor, []byte(msg))
+
+					if errWrite != nil {
+						fmt.Printf("Write error for %d: %s\n", clientWriteSocketDescriptor, errWrite.Error())
+					}
+
+				}
+
+			}
+
+		}
 
 	}
 }
